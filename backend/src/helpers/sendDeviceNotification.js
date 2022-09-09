e46ea4944d8be9e32f30a81d15c9df6ad0a8db37
Melvin <76851489+Y3KC3@users.noreply.github.com>
2022-09-09 15:05:33 -0400

    Add files via upload

---
+++ backend/src/helpers/sendDeviceNotification.js
@@ -0,0 +1,71 @@
+const { Expo } = require("expo-server-sdk");
+const Device = require("../models/Device");
+
+const expo = new Expo(/*{ accessToken: process.env.EXPO_ACCESS_TOKEN }*/);
+
+module.exports = async ({ name }) => {
+  const devices = await Device.find();
+
+  let messages = [];
+  for (let pushToken of devices) {
+    pushToken = pushToken.device;
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    messages.push({
+      to: pushToken,
+      sound: "default",
+      title: "📲 Hay una nueva publicación 📬",
+      body: `${name} esta esperando una respuesta`
+    });
+  }
+
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+  (async () => {
+    for (let chunk of chunks) {
+      try {
+        let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        console.log(ticketChunk);
+        tickets.push(...ticketChunk);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+
+  let receiptIds = [];
+  for (let ticket of tickets) {
+    if (ticket.id) {
+      receiptIds.push(ticket.id);
+    }
+  }
+
+  let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+  (async () => {
+    for (let chunk of receiptIdChunks) {
+      try {
+        let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+        console.log(receipts);
+
+        for (let receiptId in receipts) {
+          let { status, message, details } = receipts[receiptId];
+          if (status === "ok") {
+            continue;
+          } else if (status === "error") {
+            console.error(
+              `There was an error sending a notification: ${message}`
+            );
+            if (details && details.error) {
+              console.error(`The error code is ${details.error}`);
+            }
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+};
