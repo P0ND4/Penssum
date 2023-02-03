885f9dec034d34aa828fd3a20a2ea8aaa144326c
Melvin Colmenares <76851489+Y3KC3@users.noreply.github.com>
2023-02-03 14:10:19 -0400

    Delete frontend directory

--- frontend/src/components/layouts/LoadingZone.js
+++
@@ -1,8 +0,0 @@
-import { Fragment } from 'react';
-import Loading from '../parts/Loading';
-
-function LoadingZone ({ children, auth, products }){ 
-    return products !== null && auth !== null ? <Fragment>{children}</Fragment> : <Loading center={true}/>; 
-};
-
-export default LoadingZone;
