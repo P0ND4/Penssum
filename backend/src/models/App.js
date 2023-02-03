d17efc364f87f2490ba83deac9060d77c0d9d1cd
Melvin Colmenares <76851489+Y3KC3@users.noreply.github.com>
2023-02-03 14:06:26 -0400

    Delete backend directory

--- backend/src/models/App.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const AppSchema = new Schema({ 
-  id: { type: String, required: true },
-  expo: { type: String, required: true },
-  creationDate: { type: Date, default: Date.now }
-});
-
-module.exports = model("app", AppSchema);
--- backend/src/models/Block.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const BlockSchema = new Schema ({
-    from: { type: String, require: true },
-    to: { type: String, require: true },
-    creationDate: { type: Date, default: Date.now }
-});
-
-module.exports = model('block', BlockSchema);
